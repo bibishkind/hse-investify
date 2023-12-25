1d6a5ea1b68dd12681f10bd4df9ba0cbf32e5843
Denis Bibishkin <dmbibishkin@gmail.com>
2023-12-25 10:57:08 +0300

    hse-investify-app fix 7

--- hse-investify-api/app/schemas.py
+++ hse-investify-api/app/schemas.py
@@ -19,3 +19,10 @@ class NotificationsPut(BaseModel):
     ticker: str
     indicator_name: str
     expected_indicator_value: float
+
+class InformationGet(BaseModel):
+    ticker: str
+    name: str
+    price: float
+    currency: str
+    exchange: str
